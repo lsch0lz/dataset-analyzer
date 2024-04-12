41efdb7c2f5f0ed8681dc2c05fecec6b1684f9c3
Lukas Scholz <lukas.scholz@deutschebahn.com>
2024-04-12 22:26:25 +0200

    state for slider changes

--- pages/1_Similarity_Search.py
+++ pages/1_Similarity_Search.py
@@ -16,10 +16,27 @@ st.write("""First you need to enter the local path to your dataset. \n
     |--image_2.jpg
     |--image_3.jpeg""")
 
+col1, col2 = st.columns(2)
+
+if "loaded_images" not in st.session_state:
+    st.session_state.loaded_images = False
+
+if "similarity_threshold" not in st.session_state:
+    st.session_state.similarity_threshold = 0
+
 with st.form("s3_path"):
     local_image_path = st.text_input("Enter a valid local path here:")
     submit_button_image_path = st.form_submit_button("Import Images")
 
+similarity_threshold = st.sidebar.slider("Similarity Threshold", min_value=0.0, max_value=1.0, step=0.1, key="similarity_threshold")
+
 if submit_button_image_path:
-    for image in DataLoader(image_path=local_image_path).get_similar_images():
-        st.image(image)
+    st.session_state.loaded_images = True
+
+# Display images if they are loaded
+if st.session_state.loaded_images:
+    for image in DataLoader(image_path=local_image_path, similarity_threshold=similarity_threshold).get_similar_images():
+        with col1:
+            st.image(image[0])
+        with col2:
+            st.image(image[1])
